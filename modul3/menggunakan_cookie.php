90c84e0828897ac1d45c8d6bc70d7807443ad6b7
dwi heru budi <dwiheru18@gmail.com>
2013-10-24 00:25:52 +0700

    modul3
    Modul 03 Cookie dan Session

---
+++ modul3/menggunakan_cookie.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html> 
+<html> 
+ 
+<head> 
+  <title>Set Cookie</title> 
+</head> 
+ 
+<body> 
+ 
+<?php 
+ 
+// Men-set nilai cookie 
+setcookie('nama_cookie', 'nilai_cookie'); 
+ 
+// Mendapatkan nilai cookie 
+echo $_COOKIE['nama_cookie']; 
+ 
+?> 
+ 
+<p> 
+Tekan Refresh (F5); 
+ 
+</body> 
+ 
+</html> 
